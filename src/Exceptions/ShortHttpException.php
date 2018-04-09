fb5cc83a4aede7e58ca95fdf88658a1a6aae61b3
Jess Carlos <jess@ignittion.com>
2018-04-08 23:42:12 -0700

    custom exception

---
+++ src/Exceptions/ShortHttpException.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ignittion\Short\Exceptions;
+
+use Exception;
+use GuzzleHttp\Psr7\Response;
+
+class ShortHttpException extends Exception
+{
+    /**
+     * A user-friendly exception to through if we receive an error from GuzzleHttp.
+     *
+     * @param \GuzzleHttp\Psr7\Response $response
+     * @param integer $code
+     * @param \Exception $previous
+     */
+    public function __construct(Response $response, int $code = 0, Exception $previous = null)
+    {
+        $message    = json_decode($response->getBody()->__toString());
+
+        parent::__construct($message->error, $code, $previous);
+    }
+}
